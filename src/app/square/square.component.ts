4acf14321e06a2650ddf35c971075ac0fe1a712b
Aidan McBride <aidankmcbride@gmail.com>
2020-11-30 14:57:32 -0500

    styling wip

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -8,8 +8,9 @@ import { Component, Input } from '@angular/core';
     <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: [
-  ]
+
+    styleUrls: ['./square.component.scss']
+
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
